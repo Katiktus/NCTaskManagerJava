282bd1beb4abad4f30d37fd8e6d21629b9d6b78e
Kate <60921902+Katiktus@users.noreply.github.com>
2021-01-27 11:07:22 +0200

    Update Notification.java

--- src/main/java/ua/edu/sumdu/j2se/pohorila/notification/Notification.java
+++ src/main/java/ua/edu/sumdu/j2se/pohorila/notification/Notification.java
@@ -1,11 +1,8 @@
 package ua.edu.sumdu.j2se.pohorila.notification;
 
 import ua.edu.sumdu.j2se.pohorila.tasks.AbstractTaskList;
-import ua.edu.sumdu.j2se.pohorila.tasks.Task;
-
 import java.awt.*;
 import java.net.MalformedURLException;
-import java.util.Set;
 
 public interface Notification{
 		public void notify(AbstractTaskList tasks) throws MalformedURLException, AWTException;
